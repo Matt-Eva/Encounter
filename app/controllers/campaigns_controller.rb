ff671bf632205e31c319b76db0ff7ab4ae25e2ee
Garrett <garres4@vt.edu>
2022-01-04 11:24:29 -0700

    Trying to merge the controller changes in the db for campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,8 @@
 class CampaignsController < ApplicationController
+
+    def show
+        campaign = Campaign.find(params[:id])
+        render json: campaign
+    end
+
 end
--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,4 +1,6 @@
 class CampaignSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image, :status
   has_one :user
+  has_many :encs
+
 end
