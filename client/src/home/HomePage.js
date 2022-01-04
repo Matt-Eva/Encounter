3948cd65ef17e0cbb7388d3a18eaf3e0e3c969d0
Matt Eva <matteweva@gmail.com>
2022-01-03 16:25:29 -0800

    some homepage layout changed

--- client/src/home/HomePage.js
+++ client/src/home/HomePage.js
@@ -1,11 +1,25 @@
 import LogoutButton from "../userAuth/LogoutButton";
+import CampaignDisplayContainer from "../campaign/CampaignDisplayContainer";
+import CampaignCard from "../campaign/CampaignCard";
+import {Row, Container, Col} from "react-bootstrap"
 
 function HomePage(){
     return(
-        <div>
-        HomePage
-        <LogoutButton/>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    Icon
+                    Menu
+                </Col>
+                <Col>
+                   Encounter
+                </Col>
+                <Col>
+                    <LogoutButton/>
+                </Col> 
+            </Row>
+        
+        </Container>
     );
 }
 
