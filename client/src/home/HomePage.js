9f2430150925f8d33314d50f383c9bfa814c9581
Garrett <garres4@vt.edu>
2022-01-06 12:08:52 -0700

    abstract filter component

--- client/src/home/HomePage.js
+++ client/src/home/HomePage.js
@@ -9,6 +9,7 @@ import {EditCampaignContext} from "../context/editCampaignState"
 import { useContext, useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import paperBackground from '../assets/paperBackground.jpg'
+import Filter from "./Filter";
 
 
 const backgroundImageStyle = {
@@ -81,14 +82,7 @@ function HomePage(){
             </Row>
             <Row className="justify-content-center">
                 <Col>
-                <form onChange={(e) => setSelected(e.target.value)}>
-                    <input type="radio" id="all" value="all" checked={selected === "all" ? true : false}/>
-                    <label for="all" style={{"padding": "2px", "marginRight": "2px"}}>All</label>
-                    <input type="radio" id="active" value="active" checked={selected === "active" ? true : false}/>
-                    <label for="active" style={{"padding": "2px", "marginRight": "2px"}}>Active</label>
-                    <input type="radio" id="archived" value="archived" checked={selected === "archived" ? true : false}/>
-                    <label for="archived" style={{"padding": "2px", "marginRight": "2px"}}>Archived</label>
-                </form>
+                  <Filter selected={selected} setSelected={setSelected}/>
                 </Col>
             </Row>
             <Row className="justify-content-center">
