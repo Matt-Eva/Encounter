afa3dcfcd77e6a0c61965ebdfc3a2d6841da6e26
Matt Eva <matteweva@gmail.com>
2022-01-06 19:22:04 -0800

    updated login and signup page styles

--- client/src/userAuth/Signup.js
+++ client/src/userAuth/Signup.js
@@ -9,7 +9,8 @@ import { UserContext } from "../context/userState"
 const formBackgroundStyle = {
     backgroundImage: `url(${paperBackground})`,
     backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover'
+    backgroundSize: 'cover',
+    padding: "50px"
 }
 
 const backgroundImageStyle = {
@@ -74,7 +75,7 @@ function Signup(){
        <Container className="mw-100">
            <Row>
                <Col sm={4} md={4} lg={4} style={formBackgroundStyle} className="vh-100">
-                   <h1>D&D Encounter</h1>
+                   <h1>D&D: Encounter</h1>
                    <Form style={{"width": "23rem"}} onChange={handleChange} onSubmit={(e) => handleSubmit(e, form)}>
                        <Form.Group>
                            <Form.Label>Username:</Form.Label>
@@ -92,10 +93,13 @@ function Signup(){
                            <Form.Label>Confirm Password:</Form.Label>
                            <Form.Control type="password" name="passwordConfirmation" placeholder="Confirm your password here." value={form.passwordConfirmation} />
                        </Form.Group>
+                       <br/>
                        <Button variant="secondary" type="submit">Sign Up</Button>
+                       <br />
+                       <br />
                    </Form>
                    {errors ? displayErrors: null }
-                   <Link to="/login">Already have an account? Login.</Link>
+                   <Link style={{"color": "black"}} to="/login">Already have an account? Login.</Link>
                </Col>
                <Col style={backgroundImageStyle}>
                </Col>
