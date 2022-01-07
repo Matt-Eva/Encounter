5b4e664e18a88a38029ed6e4f4d9389ab7485210
Matt Eva <matteweva@gmail.com>
2022-01-07 13:13:37 -0800

    formatted encounter location page

--- client/src/location/EncounterLocationPage.js
+++ client/src/location/EncounterLocationPage.js
@@ -1,17 +1,36 @@
 import {Link} from "react-router-dom"
-import {Button} from "react-bootstrap"
+import {Button, Container, Row, Col} from "react-bootstrap"
 import {useContext} from "react"
 import {SelectedEncounterContext} from "../context/selectedEncounterState"
 import CreateEncounterLocationForm from "./CreateEncounterLocationForm";
+import parchmentBackground from "../assets/parchmentBackground.jpg"
+import LogoutButton from "../userAuth/LogoutButton";
+
+const backgroundImageStyle = {
+    backgroundImage: `url(${parchmentBackground})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    minHeight: "100vh",
+}
 
 function EncounterLocationPage(){
     const {selectedEncounter} = useContext(SelectedEncounterContext)
     return(
-        <div>
-            <Link to={`/encounter/${selectedEncounter.id}`}><Button>Back to {selectedEncounter.name}</Button></Link>
-            <h1>Create a New Location!</h1>
+        <Container style={backgroundImageStyle} className="mw-100">
+            <Row>
+                <Col>
+                    <Link to={`/encounter/${selectedEncounter.id}`} style={{"textDecoration": "none", "color": "black"}}><h4>Return to {selectedEncounter.name}</h4></Link>
+                </Col>
+                <Col>
+                    <h1>Create a New Location!</h1>
+                </Col>
+                <Col style={{"textAlign": "right"}}>
+                    <LogoutButton/>
+                </Col>
+            </Row>
+            
             <CreateEncounterLocationForm/>
-        </div>
+        </Container>
     );
 }
 
