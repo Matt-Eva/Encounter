bf82a345bada066b54a90b32da90d69788baa02e
Matt Eva <matteweva@gmail.com>
2022-02-04 15:23:09 -0800

    set up action mailer with password reset functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_170959) do
+ActiveRecord::Schema.define(version: 2022_02_04_223828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,8 @@ ActiveRecord::Schema.define(version: 2022_01_03_170959) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "dm_name"
+    t.string "recovery_password_digest"
+    t.string "email"
   end
 
   add_foreign_key "campaign_player_notes", "campaigns"
